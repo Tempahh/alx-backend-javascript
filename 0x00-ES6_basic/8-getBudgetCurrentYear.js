b2457d1eb8cd5a7fce10ba294713cdb79f932a11
Prosper <prosperorjiogo1999@gmail.com>
2024-05-29 14:59:55 +0100

    Specialization task

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,10 +1,15 @@
-export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const currentYear = getCurrentYear();
-    const budget = {
-        [`income-${currentYear}`]: income,
-        [`gdp-${currentYear}`]: gdp,
-        [`capita-${currentYear}`]: capita
-    };
+#!/usr/bin/node
+
+function getCurrentYear() {
+    const date = new Date();
+    return date.getFullYear();
+}
   
+export default function getBudgetCurrentYear(income, gdp, capita) {
+    const budget = {
+      [`income-${getCurrentYear()}`]: income,
+      [`gdp-${getCurrentYear()}`]: gdp,
+      [`capita-${getCurrentYear()}`]: capita,
+    };
     return budget;
 }
